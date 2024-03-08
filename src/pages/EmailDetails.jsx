32d14fc857448704698597cdd792a8be6357c28a
Aaron Barkan <aaron.brkn@gmail.com>
2024-03-08 22:41:53 +0200

    organized email-details

--- src/pages/EmailDetails.jsx
+++ src/pages/EmailDetails.jsx
@@ -22,26 +22,25 @@ export function EmailDetails() {
     }
   }
 
-  const starBtnTxt = (email && email.isStarred) ? "Unstar" : "Star";
+  const starBtnTxt = email && email.isStarred ? "Unstar" : "Star";
 
   if (!email) return <div>Loading...</div>;
   return (
     <section className="email-details">
-      <h2>{email.subject}</h2>
+      <header>
+        <div className="action-buttons">
+          <button className="go-back-button">Go Back</button>
+          <button className="star-toggle-btn">{starBtnTxt}</button>
+          <button className="reply-btn">Reply</button>
+          <button className="delete-btn">Delete</button>
+        </div>
+        <h2>{email.subject}</h2>
+      </header>
       <div className="sub-header">
-        <h3>{email.from}</h3>
-        <aside>
-          <h4>{utilService.formatTimestamp(email.sentAt)}</h4>
-          <div className="action-buttons">
-            <button className="star-toggle-btn">{starBtnTxt}</button>
-            <button className="reply-btn">Reply</button>
-            <button className="delete-btn">Delete</button>
-          </div>
-        </aside>
+        <h3>From: {email.from}</h3>
+        <span className="date-wrapper"><h4>{utilService.formatTimestamp(email.sentAt)}</h4></span>
       </div>
-      <main>
-        {email.body}
-      </main>
+      <main>{email.body}</main>
     </section>
   );
 }
